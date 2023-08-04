931f8efa1cd2b30f43a23de12aef7aa0133709ed
haku-ohsumi <haku818181@gmail.com>
2023-08-04 23:56:36 +0900

    ポート番号を変更したよ

--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const express = require("express");
 const app = express();
 
 app.listen (5000,function(){
-  console.log("Listening on localhost port 5000")
+  console.log("Listening on localhost port 5050")
 })
