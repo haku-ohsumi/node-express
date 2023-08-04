11f329f181b07f73b677b0858bdad4b73d26e8c2
haku-ohsumi <haku818181@gmail.com>
2023-08-05 00:28:02 +0900

    htmlファイルを表示させるよ

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const coronaData = require("./coronaData.json");
 
 app.get("/", function (req, res){
-  res.send(coronaData);
+  console.log("__dirnameの中身",__dirname);
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen (5050,function(){
