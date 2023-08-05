21350cc04d5b2b99f0162cf8f1f18b5467994339
haku-ohsumi <haku818181@gmail.com>
2023-08-05 17:37:01 +0900

    PUT前の準備

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+const fs = require('fs');
 const express = require("express");
 const app = express();
 app.use (express.urlencoded({ extended: true }));
+const activities = require ("./activities.json");
 
 app.get ("/", function (req, res){
   res.sendFile(__dirname + "/index.html");
 });
 
 app.post("/autumn", function (req, res){
-  console.log ("req.bodyの中身", req.body);
+  fs.writeFile(__dirname + "/data.txt", req.body.activity, function()
+    {
+      res.send("投稿完了");
+    });
+});
+
+app.get("/update", function (req, res){
+  res.send(activities);
 });
 
 app.listen (5050,function(){
